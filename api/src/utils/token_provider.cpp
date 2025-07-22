a67b34cb82835cc54bc3f0e81c9e3ea9762639ae
artem samohvalov <artym.samohvaloff@gmail.com>
2025-07-22 10:50:41 +0400

    class token provider

--- api/src/utils/token_provider.cpp
+++ api/src/utils/token_provider.cpp
@@ -5,9 +5,26 @@
 #include <jwt-cpp/jwt.h>
 #include <jwt-cpp/traits/kazuho-picojson/defaults.h>
 
-namespace util::token_provider
+namespace util
 {
+  TokenProvider::TokenProvider()
+  {
+    
+    load_env(); 
+  }
 
+  void TokenProvider::load_env()
+  {
+    access_excepted_min = config::get_int_env("ACCESS_EXCEPTED_MIN");
+    refresh_excepted_min = config::get_int_env("REFRESH_EXCEPTED_DAYS");
+
+    access_secret = crow::utility::base64decode(
+        config::get_env("ACCESS_TOKEN_ENCODED_SECRET"));
+    refresh_secret = crow::utility::base64decode(
+        config::get_env("REFRESH_TOKEN_ENCODED_SECRET"));
+  }
+
+#if 0
   static int access_excepted_min = config::get_int_env("ACCESS_EXCEPTED_MIN");
   static int refresh_excepted_min = config::get_int_env("REFRESH_EXCEPTED_DAYS");
   static std::string access_secret = crow::utility::base64decode(
@@ -36,4 +53,5 @@ namespace util::token_provider
 
   bool validate_access(const std::string& jwt)
   {}
+#endif
 }
--- api/src/utils/token_provider.h
+++ api/src/utils/token_provider.h
@@ -3,19 +3,33 @@
 
 #include <string>
 
-namespace util::token_provider
+namespace util
 {
-  struct Claims
+  class TokenProvider
   {
-    std::string subject;
-    std::string role;    
+  public:
+    TokenProvider();
+
+    struct Claims
+    {
+      std::string subject;
+      std::string role;    
+    };
+
+    std::string generate_refresh(const Claims& claims);
+    std::string generate_access(const Claims& claims);
+
+    bool validate_refresh(const std::string& jwt);
+    bool validate_access(const std::string& jwt);
+
+  private:
+    void load_env();
+
+    static int access_excepted_min;
+    static int refresh_excepted_min;
+    static std::string access_secret;
+    static std::string refresh_secret;
   };
-
-  std::string generate_refresh(const Claims& claims);
-  std::string generate_access(const Claims& claims);
-
-  bool validate_refresh(const std::string& jwt);
-  bool validate_access(const std::string& jwt);
 }
 
 #endif
